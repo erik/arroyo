4e5368b4a3e67a0af7295eaf448febbcefb65969
boredomist <erik.price16@gmail.com>
2012-03-31 23:54:47 -0400

    Add basic string interpolation

--- src/ast/string.c
+++ src/ast/string.c
@@ -1,7 +1,10 @@
 #include <string.h>
 #include <stdio.h>
+#include <ctype.h>
+
 #include "ast.h"
 #include "util.h"
+#include "buffer.h"
 
 string_node* string_node_create(const char* string)
 {
@@ -18,19 +21,67 @@ void string_node_destroy(string_node* node)
 
 expression_node* string_node_evaluate(string_node* node, scope* scope)
 {
-  return expression_node_create(NODE_STRING,
-                                 string_node_create(node->string));
+  unsigned len = strlen(node->string);
+
+  buffer b;
+  buffer_create(&b, len);
+
+  for(unsigned i = 0; i < len; ++i) {
+    if(node->string[i] == '$') {
+      if(node->string[i+1] == '$') {
+        buffer_putc(&b, '$');
+        i++;
+      }
+      else {
+        unsigned begin = ++i;
+        while(isalnum(node->string[i]) && ++i < len);
+
+        buffer var;
+        buffer_create(&var, i - begin);
+        buffer_putsn(&var, (node->string+begin), i - begin);
+        buffer_putc(&var, '\0');
+
+        expression_node* expr = scope_get(scope, var.buf);
+        buffer_destroy(&var);
+
+        if(expr) {
+          char* str = expression_node_to_string(expr);
+          buffer_puts(&b, str);
+          if(i != len)
+            buffer_putc(&b, ' ');
+
+          free(str);
+        }
+      }
+    }
+    else
+      buffer_putc(&b, node->string[i]);
+  }
+
+  string_node* string = string_node_create(b.buf);
+
+  buffer_destroy(&b);
+
+  return expression_node_create(NODE_STRING, string);
 }
 
 expression_node* string_node_clone(string_node* node)
 {
-  return expression_node_create(NODE_STRING,
-                                string_node_create(node->string));
+  return expression_node_create(
+    NODE_STRING, string_node_create(node->string));
 }
 
 string_node* string_node_to_string_node(string_node* node)
 {
-  string_node* clone = calloc(sizeof(string_node), 1);
-  clone->string = strdup(node->string);
-  return clone;
+  buffer b;
+  buffer_create(&b, strlen(node->string) + 2);
+
+  buffer_putc(&b, '"');
+  buffer_puts(&b, node->string);
+  buffer_putc(&b, '"');
+
+  string_node* string = string_node_create(b.buf);
+
+  buffer_destroy(&b);
+  return string;
 }
